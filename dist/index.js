5961bac7b13a5f0658b65d118d50e938544f2667
Markus Wesslén <markus.wesslen@gmail.com>
2021-04-29 17:44:19 +0200

    Add parsing

--- dist/index.js
+++ dist/index.js
@@ -6061,7 +6061,21 @@ async function run() {
         ? await makeScoreboard(client, "Scoreboard")
         : scoreboardIssueList.data[0].number;
     console.log("scoreboard issue number %s", scoreboardIssueNumber);
-    await updateScoreboard(client, scoreboardIssueNumber);
+
+    const currentScores = await parseScoreboard(client, scoreboardIssueNumber);
+    console.log("--- Current scores:");
+    console.log(currentScores);
+
+    const newScores = JSON.parse(core.getInput("new-scores"));
+    console.log("--- New scores:");
+    console.log(newScores);
+
+    await updateScoreboard(
+      client,
+      scoreboardIssueNumber,
+      newScores,
+      currentScores
+    );
   } catch (error) {
     core.setFailed(error.message);
     console.log(error);
@@ -6088,7 +6102,27 @@ async function makeScoreboard(client, title) {
   return issueNumber;
 }
 
-async function updateScoreboard(client, issueNumber) {
+async function parseScoreboard(client, issueNumber) {
+  /*
+  These are the top contributors of this repo.
+
+  1. m4reko: 10
+  2. Wizkas0: 5
+  */
+  const issue = await client.issues.get({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    issue_number: issueNumber,
+  });
+  const body = issue.body;
+  const rowRegex = /^\d+\. (.+): (\d+)$/;
+  return body.split("\n").reducer((map, row) => {
+    const match = rowRegex.exec(row);
+    map.set(match[1], match[2]);
+  }, Map());
+}
+
+async function updateScoreboard(client, issueNumber, newScores, currentScores) {
   const issueBody = createIssueBody();
   await client.issues.update({
     owner: github.context.repo.owner,
@@ -6099,7 +6133,7 @@ async function updateScoreboard(client, issueNumber) {
 }
 
 function createIssueBody() {
-  return "Hello World";
+  return "1. m4reko: 10\n2. Wizkas0: 5";
 }
 
 async function pinIssue(octokit, id) {
--- index.js
+++ index.js
@@ -17,7 +17,21 @@ async function run() {
         ? await makeScoreboard(client, "Scoreboard")
         : scoreboardIssueList.data[0].number;
     console.log("scoreboard issue number %s", scoreboardIssueNumber);
-    await updateScoreboard(client, scoreboardIssueNumber);
+
+    const currentScores = await parseScoreboard(client, scoreboardIssueNumber);
+    console.log("--- Current scores:");
+    console.log(currentScores);
+
+    const newScores = JSON.parse(core.getInput("new-scores"));
+    console.log("--- New scores:");
+    console.log(newScores);
+
+    await updateScoreboard(
+      client,
+      scoreboardIssueNumber,
+      newScores,
+      currentScores
+    );
   } catch (error) {
     core.setFailed(error.message);
     console.log(error);
@@ -44,7 +58,27 @@ async function makeScoreboard(client, title) {
   return issueNumber;
 }
 
-async function updateScoreboard(client, issueNumber) {
+async function parseScoreboard(client, issueNumber) {
+  /*
+  These are the top contributors of this repo.
+
+  1. m4reko: 10
+  2. Wizkas0: 5
+  */
+  const issue = await client.issues.get({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    issue_number: issueNumber,
+  });
+  const body = issue.body;
+  const rowRegex = /^\d+\. (.+): (\d+)$/;
+  return body.split("\n").reducer((map, row) => {
+    const match = rowRegex.exec(row);
+    map.set(match[1], match[2]);
+  }, Map());
+}
+
+async function updateScoreboard(client, issueNumber, newScores, currentScores) {
   const issueBody = createIssueBody();
   await client.issues.update({
     owner: github.context.repo.owner,
@@ -55,7 +89,7 @@ async function updateScoreboard(client, issueNumber) {
 }
 
 function createIssueBody() {
-  return "Hello World";
+  return "1. m4reko: 10\n2. Wizkas0: 5";
 }
 
 async function pinIssue(octokit, id) {
