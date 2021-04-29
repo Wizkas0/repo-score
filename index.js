bf5ca2ef94e53da68ed5ff04f7f5a0dc6818fa93
Markus Wesslén <markus.wesslen@gmail.com>
2021-04-29 16:17:17 +0200

    Add update scoreboard template

--- index.js
+++ index.js
@@ -5,18 +5,19 @@ async function run() {
   try {
     const token = core.getInput("repo-token", { required: true });
     const client = github.getOctokit(token);
-    const scoreboard = await client.issues.listForRepo({
+    const scoreboardIssueList = await client.issues.listForRepo({
       owner: github.context.repo.owner,
       repo: github.context.repo.repo,
       labels: "Scoreboard",
     });
-    console.log("Scoreboard");
-    console.log(scoreboard);
-    const scoreboardId =
-      scoreboard.data.length === 0
+    console.log("--- Scoreboard issues:");
+    console.log(scoreboardIssueList);
+    const scoreboardIssueNumber =
+      scoreboardIssueList.data.length === 0
         ? await makeScoreboard(client, "Scoreboard")
-        : scoreboard.data[0].number;
-    console.log("scoreboard issue number %s", scoreboardId)
+        : scoreboardIssueList.data[0].number;
+    console.log("scoreboard issue number %s", scoreboardIssueNumber);
+    await updateScoreboard(client, scoreboardIssueNumber);
   } catch (error) {
     core.setFailed(error.message);
     console.log(error);
@@ -29,24 +30,32 @@ async function makeScoreboard(client, title) {
     repo: github.context.repo.repo,
     title: title,
   });
-
+  console.log("--- Issue:");
   console.log(issue);
   const id = issue.data.node_id;
-  const issue_nmbr = issue.data.number;
-  console.log(issue_nmbr);
-  console.log(id);
+  const issueNumber = issue.data.number;
   await client.issues.addLabels({
     owner: github.context.repo.owner,
     repo: github.context.repo.repo,
-    issue_number: issue_nmbr,
+    issue_number: issueNumber,
     labels: ["Scoreboard"],
   });
   await pinIssue(client, id);
-  return issue_nmbr;
+  return issueNumber;
 }
 
-async function updateScoreboard(client, owner, repo, id) {
+async function updateScoreboard(client, issueNumber) {
+  const issueBody = createIssueBody();
+  await client.issues.update({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    issue_number: issueNumber,
+    body: issueBody,
+  });
+}
 
+function createIssueBody() {
+  return "Hello World";
 }
 
 async function pinIssue(octokit, id) {
@@ -57,14 +66,13 @@ async function pinIssue(octokit, id) {
     }
   }
 `;
-  const pinnn = await octokit.graphql(pinMutation, {
+  await octokit.graphql(pinMutation, {
     id: id,
     input: { issueId: id },
     headers: {
       accept: "application/vnd.github.elektra-preview+json",
     },
   });
-  console.log(pinnn);
 }
 
 run();
